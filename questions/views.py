2d6d47894f72b6f008d9326d0985acf29a3afc1a
Your Name <arian.radmehr.bw@gmail.com>
2020-12-26 02:58:36 +0330

    deleted some useless comments

--- questions/views.py
+++ questions/views.py
@@ -5,21 +5,13 @@ from django.views.generic import TemplateView, ListView, DetailView, CreateView
 from django.urls import reverse_lazy
 
 
-# def index_view(request): # its home page
-#     return render(request, 'questions/index.html')
 class HomeTemplateView(TemplateView):
     template_name = 'questions/index.html'
 
 
-# def about_view(request):
-#     return render(request, 'questions/about.html')
-
 class AboutTemplateView(TemplateView):
     template_name = 'questions/about.html'
 
-# def questions_view(request):
-#     all_questions = Question.objects.all()
-#     return render(request, 'questions/posts/allPosts.html',{'all_posts': all_questions})
 
 class QuestionListView(ListView):
     model = Question
@@ -41,21 +33,7 @@ class QuestionDetailView(DetailView):
         self.object.refresh_from_db()
         return context
 
-# def add_question(request):
-#     if request.method == 'POST':
-#         question_form = QuestionForm(request.POST)
-#         if question_form.is_valid():
-#             cleaned_data = question_form.cleaned_data
-#             current_user = request.user
-#             question = Question(title=cleaned_data['title'], text=cleaned_data['text'], category=cleaned_data['category'], user=current_user)
-#             question.save()
-#             return redirect('questions:all_questions')
 
-#     else:
-#         question_form = QuestionForm()
-#         return render(request,'questions/posts/add_question.html', {'QuestionForm': question_form})
-
-#
 # class QuestionCreateView(CreateView):
 #     model = Question
 #     form_class = QuestionForm
@@ -88,13 +66,10 @@ class QuestionCreateView(CreateView):
 class CategoryQuestionsDetailView(DetailView):
     model = Category
     template_name = 'questions/categories/category_questions.html'
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        # context['category'] = Category.objects.get(id=pk)
-        context['cat_ques'] = context['category'].category_questions.all()
-        # context['questions'] = Question.objects.all()
-        # print("CATEGORY TITLE: ", context['category'].title)
-        # print("QUESTION_TITLE: ", context['cat_ques'][1].title)
+        context['cat_ques'] = context['category'].category_questions.all()  # related_name = category_questions
 
         return context
 
